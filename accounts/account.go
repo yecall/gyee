e0a2f22711e571308ef0120d53156bfea6f6c5a5
Xu Jiajun <xujiajun@yeecall.com>
2018-04-08 19:40:21 +0800

    test acc

---
+++ accounts/account.go
@@ -0,0 +1,63 @@
+package accounts
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"log"
+	"crypto/sha256"
+	"golang.org/x/crypto/ripemd160"
+	"github.com/mr-tron/base58/base58"
+)
+
+const version = byte(0x00)
+const addressChecksumLen = 4
+
+type Account struct {
+	PrivateKey ecdsa.PrivateKey
+	PublicKey []byte
+}
+
+func NewAccount() *Account {
+	private, public := newKeyPair()
+	account := Account{private, public}
+	return &account
+}
+
+func newKeyPair() (ecdsa.PrivateKey, []byte) {
+	curve := elliptic.P256()
+	private, err := ecdsa.GenerateKey(curve, rand.Reader)
+	if err != nil {
+		log.Panic(err)
+	}
+	pubKey := append(private.PublicKey.X.Bytes(), private.PublicKey.Y.Bytes()...)
+	return *private, pubKey
+}
+
+func (a Account) GetAddress() []byte {
+    pubKeyHash := HashPubKey(a.PublicKey)
+    versionedPayload := append([]byte{version}, pubKeyHash...)
+    checksum := checksum(versionedPayload)
+    fullPayload := append(versionedPayload, checksum...)
+    address := base58.Encode(fullPayload)
+
+    return []byte(address)
+}
+
+func HashPubKey(pubKey []byte) []byte {
+	publicSHA256 := sha256.Sum256(pubKey)
+	RIPEMD160Hasher := ripemd160.New()
+	_, err := RIPEMD160Hasher.Write(publicSHA256[:])
+	if err != nil {
+		log.Panic(err)
+	}
+	publicRIPEMD160 := RIPEMD160Hasher.Sum(nil)
+
+	return publicRIPEMD160
+}
+
+func checksum(payload []byte) []byte{
+	firstSHA := sha256.Sum256(payload)
+	secondSHA := sha256.Sum256(firstSHA[:])
+	return secondSHA[:addressChecksumLen]
+}
---
+++ accounts/accounts.go
@@ -0,0 +1,86 @@
+package accounts
+
+import (
+	"log"
+	"os"
+	"io/ioutil"
+	"encoding/gob"
+	"crypto/elliptic"
+	"bytes"
+	"fmt"
+)
+
+const keystoreFile = "keystore.dat"
+
+type Accounts struct {
+	Accounts map[string]*Account
+}
+
+
+func AccountsManager() (*Accounts){
+	accounts := Accounts{}
+	accounts.Accounts = make(map[string]*Account)
+	err := accounts.LoadFromFile()
+	if err != nil {
+		log.Println(err)
+	}
+	return &accounts
+}
+
+func (acc *Accounts)NewAccount() (*Account) {
+	account := NewAccount()
+	address := fmt.Sprintf("%s", account.GetAddress())
+	acc.Accounts[address] = account
+	acc.SaveToFile()
+	return account
+}
+
+func (acc *Accounts)GetAccount(address string) (*Account) {
+	return acc.Accounts[address]
+}
+
+func (acc *Accounts)GetAddresses() []string {
+	var addresses []string
+	for address := range acc.Accounts {
+		addresses = append(addresses, address)
+	}
+	return addresses
+}
+
+func (acc *Accounts)LoadFromFile() error {
+    if _, err := os.Stat(keystoreFile); os.IsNotExist(err) {
+    	return err
+	}
+
+	fileContent, err := ioutil.ReadFile(keystoreFile)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	var accounts Accounts
+	gob.Register(elliptic.P256())
+	decoder := gob.NewDecoder(bytes.NewReader(fileContent))
+	err = decoder.Decode(&accounts)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	acc.Accounts = accounts.Accounts
+	return nil
+}
+
+func (acc *Accounts)SaveToFile() {
+	var content bytes.Buffer
+
+	gob.Register(elliptic.P256())
+	encoder := gob.NewEncoder(&content)
+	err := encoder.Encode(acc)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	err = ioutil.WriteFile(keystoreFile, content.Bytes(), 0644)
+	if err != nil {
+		log.Panic(err)
+	}
+}
