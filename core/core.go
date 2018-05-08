1a3e84e6a7e09b94f3092cc8defbc6f4f43193b2
Xu Jiajun <xujiajun@yeecall.com>
2018-05-08 18:03:02 +0800

    comm

--- core/core.go
+++ core/core.go
@@ -3,12 +3,12 @@
  *
  *  This file is part of the gyee library.
  *
- *  the gyee library is free software: you can redistribute it and/or modify
+ *  The gyee library is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  the gyee library is distributed in the hope that it will be useful,
+ *  The gyee library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
@@ -49,6 +49,7 @@ type Core struct {
 	config *config.Config
 	tetris *tetris.Tetris
 	tetrisOutputCh chan tetris.ConsensusOutput
+
 }
 
 func NewCore(node INode, conf *config.Config) (*Core, error) {
