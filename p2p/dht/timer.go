ee92b6f251370630b26d3378b731a531f5aaec3e
liyongyiyy <liyongyi@yeecall.com>
2018-11-29 09:22:48 +0800

    Add skeleton for dht timer manager

--- p2p/dht/timer.go
+++ p2p/dht/timer.go
@@ -19,3 +19,65 @@
  */
 
 package dht
+
+import (
+	"time"
+	"container/list"
+)
+
+const (
+	secondCycle		= 64			// cycle in seconds
+	minuteCycle		= 64			// cycle in minutes
+	hourCycle		= 64			// cycle in hours
+	dayCycle		= 64			// cycle in days
+	oneTick			= time.Second	// unit tick to driver the timer manager
+)
+
+type timerCallback = func(el *list.Element, para interface{})interface{}
+
+type timer struct {
+	s			int				// seconds remain
+	m			int				// minutes remain
+	h			int				// hours remain
+	d			int				// day remain
+	data		interface{}		// pointer passed to callback
+	tcb			timerCallback	// callback when timer expired
+}
+
+type timerManager struct {
+	sp			int						// second pointer
+	mp			int						// minute pointer
+	hp			int						// hour pointer
+	dp			int						// day pointer
+	sTmList		[secondCycle]*list.List	// second timer list
+	mTmList		[minuteCycle]*list.List	// minute timer list
+	hTmList		[hourCycle]*list.List	// hour timer list
+	dTmList		[dayCycle]*list.List	// day timer list
+}
+
+func getTimer(dur time.Duration, dat interface{}, tcb timerCallback) (*timer, error) {
+	return nil, nil
+}
+
+func setTimerHandler(tm *timer, tcb timerCallback) error {
+	return nil
+}
+
+func setTimerData(tm *timer, data interface{}) error {
+	return nil
+}
+
+func setTimer(tm *timer) (*list.Element, error) {
+	return nil, nil
+}
+
+func killTimer(el *list.Element) error {
+	return nil
+}
+
+func tickProc() error {
+	return nil
+}
+
+
+
