6f230e73841d7d814017d182096e1861af81c0f0
sam4it <sam4it@gmail.com>
2018-12-25 15:28:33 +0800

    Implement p2p osn service interface

--- p2p/osn_service.go
+++ p2p/osn_service.go
@@ -23,3 +23,66 @@
 */
 
 package p2p
+
+import (
+	"github.com/pkg/errors"
+	p2psh "github.com/yeeco/gyee/p2p/shell"
+)
+
+type OsnService struct {
+	yeShCfg		p2psh.YeShellConfig
+	yeShMgr		Service
+}
+
+func OsnServiceConfig(cfg *p2psh.YeShellConfig) error {
+	return nil
+}
+
+func NewOsnService()(*OsnService, error) {
+	osns := OsnService{
+		yeShCfg: p2psh.DefaultYeShellConfig,
+	}
+	OsnServiceConfig(&osns.yeShCfg)
+	if osns.yeShMgr = p2psh.NewYeShellManager(&osns.yeShCfg); osns.yeShMgr == nil {
+		return nil, errors.New("NewOsnService: NewYeShellManager failed")
+	}
+	return &osns, nil
+}
+
+func (osns *OsnService)Start() error {
+	return osns.yeShMgr.Start()
+}
+
+func (osns *OsnService)Stop() {
+	osns.yeShMgr.Stop()
+}
+
+func (osns *OsnService)Reconfig(reCfg *RecfgCommand) error {
+	return osns.yeShMgr.Reconfig(reCfg)
+}
+
+func (osns *OsnService)BroadcastMessage(message Message) error {
+	return osns.BroadcastMessage(message)
+}
+
+func (osns *OsnService)BroadcastMessageOsn(message Message) error {
+	return osns.BroadcastMessageOsn(message)
+}
+
+func (osns *OsnService)Register(subscriber *Subscriber) {
+	osns.Register(subscriber)
+}
+
+func (osns *OsnService)UnRegister(subscriber *Subscriber) {
+	osns.UnRegister(subscriber)
+}
+
+func (osns *OsnService)DhtGetValue(key []byte) ([]byte, error) {
+	return osns.yeShMgr.DhtGetValue(key)
+}
+
+func (osns *OsnService)DhtSetValue(key []byte, value []byte) error {
+	return osns.yeShMgr.DhtSetValue(key, value)
+}
+
+
