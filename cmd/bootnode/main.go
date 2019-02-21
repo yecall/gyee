135451afc45aff797a07b36f8ca21656a49083bb
Chang Ming <pkuchm@gmail.com>
2019-02-21 16:46:51 +0800

    cmd: bootnode: prepare nodeKey

--- cmd/bootnode/main.go
+++ cmd/bootnode/main.go
@@ -17,5 +17,56 @@
 
 package main
 
+import (
+	"crypto/ecdsa"
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/yeeco/gyee/log"
+	p2pcfg "github.com/yeeco/gyee/p2p/config"
+)
+
 func main() {
+	var (
+		genKey      = flag.String("genkey", "", "generate node key to file")
+		writeNodeID = flag.Bool("writenodeid", false, "write out the node's id and quit")
+		nodeKeyFile = flag.String("nodekey", "", "private key filename")
+
+		nodeKey *ecdsa.PrivateKey
+		err     error
+	)
+	flag.Parse()
+
+	switch {
+	case *genKey != "":
+		nodeKey, err = p2pcfg.GenerateKey()
+		if err != nil {
+			log.Crit("failed to generate nodekey", "err", err)
+		}
+		if err = p2pcfg.SaveECDSA(*genKey, nodeKey); err != nil {
+			log.Crit("failed to save nodekey", "err", err)
+		}
+		return
+	case *nodeKeyFile != "":
+		nodeKey, err = p2pcfg.LoadECDSA(*nodeKeyFile)
+		if err != nil {
+			log.Crit("failed to load nodekey", "err", err)
+		}
+	}
+
+	if nodeKey == nil {
+		log.Crit("nodeKey not provided")
+	}
+
+	if *writeNodeID {
+		nodeID := p2pcfg.P2pPubkey2NodeId(&nodeKey.PublicKey)
+		if nodeID == nil {
+			log.Crit("failed to parse nodeID")
+		}
+		fmt.Printf("%x\n", *nodeID)
+		os.Exit(0)
+	}
+
+	// TODO: start node with nodeKey
 }
