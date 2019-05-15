0d2c97091c499b40e97bb63ad12c7c4160cb84d3
Chang Ming <pkuchm@gmail.com>
2019-05-15 20:02:27 +0800

    p2p: missing channel init

--- p2p/inmem_service.go
+++ p2p/inmem_service.go
@@ -56,6 +56,7 @@ func NewInmemService() (*InmemService, error) {
 		subscribers:      new(sync.Map),
 		hub:              GetInmemHub(),
 		receiveMessageCh: make(chan Message),
+		readyCh:          make(chan struct{}),
 		quitCh:           make(chan struct{}),
 		outDelay:         1,
 		outMiss:          0,
