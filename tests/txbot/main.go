9edfcbde66c61da9ccc686a91ce2b9e5e49082a1
Chang Ming <pkuchm@gmail.com>
2019-05-16 13:31:37 +0800

    txbot: slow down

--- tests/txbot/main.go
+++ tests/txbot/main.go
@@ -162,11 +162,11 @@ func genTxs(n *node.Node, signers []crypto.Signer, addrs []common.Address, quitC
 	// generator loop
 	round := int(0)
 	totalTxs := int(0)
-	ticker := time.NewTicker(250 * time.Millisecond)
+	ticker := time.NewTicker(1000 * time.Millisecond)
 Exit:
 	for {
 		// reset inMem nonce if needed
-		if round%600 == 0 {
+		if round%120 == 0 {
 			log.Info("nonce reset")
 			c.TriggerSync()
 			func() {
