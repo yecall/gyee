05bfb4fb15d4389accbf6c2e8b51218210fea11f
Chang Ming <pkuchm@gmail.com>
2019-05-17 16:11:58 +0800

    txbot: more nonce reset

--- tests/txbot/main.go
+++ tests/txbot/main.go
@@ -166,7 +166,7 @@ func genTxs(n *node.Node, signers []crypto.Signer, addrs []common.Address, quitC
 Exit:
 	for {
 		// reset inMem nonce if needed
-		if round%120 == 0 {
+		if round%10 == 0 {
 			log.Info("nonce reset")
 			c.TriggerSync()
 			func() {
